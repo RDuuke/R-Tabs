5c647fb0b364bd94e133bf0259ffa6a2da42e9d4
rduuke <juuanduuke@gmail.com>
2019-02-14 15:31:54 -0500

    first commit

---
+++ demo/js/t-tabs.js
@@ -0,0 +1,24 @@
+var link_tabs = document.getElementsByClassName('link-tab');
+var item_tabs = document.getElementsByClassName("item-tab");
+for (e = 0; e < link_tabs.length; e++) {
+    link_tabs[e].addEventListener("click", function (event) {
+        event.preventDefault();
+            displayTab(this.getAttribute("data-item"));
+            activeMenu();
+            this.classList.add("active");
+        }, false);
+    }
+
+function displayTab(element) {
+    for (i = 0; i < item_tabs.length; i++) {
+        item_tabs[i].classList.remove("active");
+    }
+    document.getElementById(element).classList.add("active");
+}
+
+function activeMenu() {
+    for (a = 0; a < link_tabs.length; a++) {
+       link_tabs[a].classList.remove("active");
+    }
+}
+
